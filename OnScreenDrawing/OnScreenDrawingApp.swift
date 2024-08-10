0f48f47999b9fb3142151d87d6c55b092f4ba4b9
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-08-10 09:39:48 +0300

    Initial Commit

---
+++ OnScreenDrawing/OnScreenDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnScreenDrawingApp.swift
+//  OnScreenDrawing
+//
+//  Created by Ramill Ibragimov on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OnScreenDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
